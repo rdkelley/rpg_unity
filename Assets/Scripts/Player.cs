29378e61c707560133ec46aec1c49c9dc947a196
Ryan Kelley <rdkelley@gmail.com>
2024-05-28 22:19:40 -0700

    Add PLayer, player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float baseStr, baseDef, baseInt;
+    [SerializeField] Attribute strength, defense, intelligence;
+
+    [SerializeField] Dictionary<Attribute, Wrapper> stats = new Dictionary<Attribute, Wrapper>();
+
+    public T Get<T>(Attribute attribute) where T : Wrapper
+    {
+        if (!stats.ContainsKey(attribute))
+            return null;
+        return stats[attribute] as T;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
