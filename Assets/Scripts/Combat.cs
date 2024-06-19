9ef3d52f7547e504abd4ebf7fde919c73fa6d21a
Ryan Kelley <kelleyr@eastonrye.com>
2024-06-19 14:54:29 -0700

    Add combat script, animations

---
+++ Assets/Scripts/Combat.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+
+public class Combat : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    //[SerializeField] Weapon weapon;
+    [SerializeField] ThirdPersonController controller;
+
+    private void OnValidate()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<ThirdPersonController>();
+    }
+
+    //Should not attack if already attacking
+    bool attacking;
+    bool Attacking
+    {
+        set
+        {
+            attacking = value;
+
+            //Disable the controller so the character does not move while attacking
+            controller.enabled = !value;
+        }
+    }
+
+    //This function is called when the player presses LMB
+    public void OnAttack()
+    {
+        Debug.Log("Attack");
+        Debug.Log(attacking);
+        if (attacking)
+            return;
+
+        //weapon = GetComponentInChildren<Weapon>();
+        animator.SetTrigger("Attack");
+    }
+
+    //An animation event raised when the sword can damage others
+    public void Draw()
+    {
+        Attacking = true;
+        //weapon.Activate();
+    }
+
+    //An animation event raised when attack animation is done
+    public void Sheathe()
+    {
+        //weapon.Deactivate();
+        Attacking = false;
+        animator.ResetTrigger("Attack");
+    }
+}
