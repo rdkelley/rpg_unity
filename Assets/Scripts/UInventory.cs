382daa6ab3f700af9afb6fc49bc3e1a2615cda62
Ryan Kelley <kelleyr@eastonrye.com>
2024-08-02 15:15:11 -0700

    Fix scroll bar

--- Assets/Scripts/UInventory.cs
+++ Assets/Scripts/UInventory.cs
@@ -7,19 +7,26 @@ public class UInventory : MonoBehaviour
     public List<UItem> items = new List<UItem>();
     public GameObject inventoryPanel;
     public GameObject uItemPrefab;
+    public GameObject potionPrefab;
+    public GameObject swordPrefab;
 
     [SerializeField] Inventory playerInventory;
+
+    Dictionary<string, GameObject> itemVariants = new Dictionary<string, GameObject>();
 
     void OnEnable()
     {
         playerInventory.onAddItem += Add;
+
+        itemVariants.Add("Potion", potionPrefab);
+        itemVariants.Add("Sword", swordPrefab);
     }
 
     void Add(Enums item)
     {
         Debug.Log("ITEM ADDED: " + item.ToString());
 
-        GameObject slot = Instantiate(uItemPrefab, inventoryPanel.transform);
+        GameObject slot = Instantiate(itemVariants[item.ToString()], inventoryPanel.transform);
 
         // Set slot properties like position, icon, etc.
         slot.GetComponent<UItem>().Set(item);
