51ed1966043d28a996c2e9d330daa2ac971e0d8c
Ryan Kelley <kelleyr@eastonrye.com>
2024-08-03 16:13:46 -0700

    Remove second audio listener

--- Assets/Scripts/Consumable.cs
+++ Assets/Scripts/Consumable.cs
@@ -6,6 +6,7 @@ public class Consumable : Item
 {
     public override void Use(Player player)
     {
+        Debug.Log("USE RAN");
         var notifier = player.Get<Notifier>(attribute) as Notifier;
         notifier.Add(Amount);
     }
