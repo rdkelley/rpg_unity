bdd62f1424847a60b399c739ea14145def433a8c
Ryan Kelley <kelleyr@eastonrye.com>
2024-08-03 17:54:07 -0700

    Add button click

--- Assets/Scripts/Consumable.cs
+++ Assets/Scripts/Consumable.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Consumable : Item
 {
+
     public override void Use(Player player)
     {
-        Debug.Log("USE RAN");
+        Debug.Log("THIS RAN!");
         var notifier = player.Get<Notifier>(attribute) as Notifier;
         notifier.Add(Amount);
     }
